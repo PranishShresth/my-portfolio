93c3dbbaac09b84104ef9416541620366f547223
PranishShresth <shresthapranish99@gmail.com>
2020-09-18 21:43:23 +1000

    amended navlink error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -59,21 +59,17 @@ const Header = () => {
             <Close onClick={handleHamburger} />
           </div>
           <li>
-            <a href="#projects" className="nav_link" onClick={handleHamburger}>
+            <a href="#projects" onClick={handleHamburger}>
               Projects
             </a>
           </li>
           <li>
-            <a href="#about_me" className="nav_link" onClick={handleHamburger}>
+            <a href="#about_me" onClick={handleHamburger}>
               About
             </a>
           </li>
           <li>
-            <a
-              href="#skills_container"
-              className="nav_link"
-              onClick={handleHamburger}
-            >
+            <a href="#skills_container" onClick={handleHamburger}>
               Skills
             </a>
           </li>
